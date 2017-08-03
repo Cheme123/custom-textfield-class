a34450d37e6b3ef555a41c35badc691011fc0136
Miguel Altamirano <miguelaltamirano@dhcp-206-12-45-218.ubcsecure.wireless.ubc.ca>
2017-08-03 14:52:56 -0700

    Initial Commit

---
+++ custom-textfield-class-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  custom-textfield-class-test
+//
+//  Created by Miguel Altamirano on 2017-08-03.
+//  Copyright © 2017 Miguel Altamirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
